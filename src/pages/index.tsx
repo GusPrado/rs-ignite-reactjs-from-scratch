ca0ad0fe95190311d15103b8118b122628b0629a
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2022-06-15 00:03:32 -0300

    feat: Add posts from Prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { GetStaticProps } from 'next';
-
+import { RichText } from 'prismic-dom';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,24 +24,51 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home(): JSX.Element {
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  const { results, next_page } = postsPagination;
+
   return (
     <main className={styles.contentContainer}>
-      <article className={styles.postContainer}>
-        <h3>titulo do post</h3>
-        <p>texto do post</p>
-      </article>
-      <article className={styles.postContainer}>
-        <h3>titulo do post</h3>
-        <p>texto do post</p>
-      </article>
+      {results.map(post => (
+        <article key={post.uid} className={styles.postContainer}>
+          <h2>{post.data.title}</h2>
+          <p>{post.data.subtitle}</p>
+          <section>
+            <time>{post.first_publication_date}</time>
+            <span>{post.data.author}</span>
+          </section>
+        </article>
+      ))}
     </main>
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const postsResponse = await prismic.getByType('posts');
 
-//   // TODO
-// };
+  console.log(JSON.stringify(postsResponse, null, 2));
+
+  const { next_page } = postsResponse;
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  return {
+    props: {
+      postsPagination: {
+        next_page,
+        results: posts,
+      },
+    },
+  };
+};
